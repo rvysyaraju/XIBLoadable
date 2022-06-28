1d3d3a70167782bd158d76e5590ffb871a44758f
Raviraju Vysyaraju <raviraju.vysyaraju@cyberark.com>
2022-06-28 21:02:09 +0530

    initial commit

---
+++ XIBLoadable.xcframework/ios-arm64_x86_64-simulator/SampleTestSDK.framework/Headers/SampleTestSDK.h
@@ -0,0 +1,18 @@
+//
+//  SampleTestSDK.h
+//  SampleTestSDK
+//
+//  Created by Raviraju Vysyaraju on 28/06/22.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SampleTestSDK.
+FOUNDATION_EXPORT double SampleTestSDKVersionNumber;
+
+//! Project version string for SampleTestSDK.
+FOUNDATION_EXPORT const unsigned char SampleTestSDKVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SampleTestSDK/PublicHeader.h>
+
+
